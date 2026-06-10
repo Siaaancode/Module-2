f06a54220c78b4f801f1b2cb5473d119a72bb281
siaaancode <s.sherman96@hotmail.co.uk>
2026-06-10 22:59:06 +0100

    Created the functions for the income and expense calculator sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
 /* Adds new input when icon is clicked */
-/* Code for incomes */
 function addInputIncome () {
     const newRow = document.createElement("div");
 
     newRow.innerHTML = `
     <input type="text" placeholder="e.g. Salary">
     <label>£</label>
-    <input type="number" placeholder="Amount">
+    <input class="amount-number-income" type="number" placeholder="Amount" oninput="incomeTotal()" >
     `;
 
     const container = document.getElementById("income-list");
@@ -22,7 +21,7 @@ function addInputExpense () {
     newRow.innerHTML = `
     <input type="text" placeholder="e.g. Food">
     <label>£</label>
-    <input type="number" placeholder="Amount">
+    <input class="amount-number-expense" type="number" placeholder="Amount" oninput="expenseTotal()">
     `;
 
     const container = document.getElementById("expense-list");
@@ -33,17 +32,40 @@ document.getElementById("icon-expense").addEventListener("click", addInputExpens
 
 
 /* Calculates the total of income */
-/* Insert code here */
 function incomeTotal() {
-    let incomeArray = document.getElementById ("amountNumber");
+
+    const incomes = document.getElementsByClassName ("amount-number-income");
+    const totalIncomeInput = document.getElementById ("total-income-input");
+
+    let total = 0;
+
+    for (let i = 0; i < incomes.length; i++) {
+        total += Number(incomes[i].value) || 0;
+    }
+
+    totalIncomeInput.value = total;
 }
 
 
 /* Calculates the total of expenses */
-/* Insert code here */
+
 function expenseTotal() {
 
+    const expenses = document.getElementsByClassName ("amount-number-expense");
+    const totalExpenseInput = document.getElementById ("total-expense-input");
+
+    let total = 0;
+
+    for (let i = 0; i < expenses.length; i++) {
+        total += Number(expenses[i].value) || 0;
+    }
+
+    totalExpenseInput.value = total;
 }
+
+
+
+
 /* Calculates the total of income both income and expenses */
 /* Insert code here */
 function breakdownOfTotals() {
